f1cad78b340883b8e6a534be9bf9e90688a69323
SionHu <hu440@purdue.edu>
2019-11-22 22:42:18 -0500

    Add scrollable nav bar template

--- mcb/settings.py
+++ mcb/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
 
-ALLOWED_HOSTS = ['purdue-mcb.herokuapp.com', 'localhost:8000']
+ALLOWED_HOSTS = ['purdue-mcb.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
