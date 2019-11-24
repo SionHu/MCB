9010ef1ca7c0badbb0587534bd7f574b1a07292d
SionHu <hu440@purdue.edu>
2019-11-24 18:11:07 -0500

    web update

--- core/views.py
+++ core/views.py
@@ -5,15 +5,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def homepage(request):
-    # return HttpResponse("hello world")
     return render_to_response('homepage.html')
 
 
 def radial(request):
-    # return HttpResponse("hello world")
     return render_to_response('radial_tree.html')
 
 
 def hori(request):
-    # return HttpResponse("hello world")
     return render_to_response('hori_tree.html')
