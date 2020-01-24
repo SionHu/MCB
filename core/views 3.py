937de33a4ad4ba7c5330bcc8d6c1b7be0a51ea21
SionHu <hu440@purdue.edu>
2020-01-23 19:06:11 -0500

    fix the blank right margin problem

---
+++ core/views 3.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+
+# Create your views here.
+
+def homepage(request):
+    return render_to_response('homepage.html')
+
+
+def radial(request):
+    return render_to_response('radial_tree.html')
+
+
+def hori(request):
+    return render_to_response('hori_tree.html')
